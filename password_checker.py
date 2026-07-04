e2b691f5e48d8871d6b6546abde546bb41e52b55
Brindha R <brindharadhakrishnan13@gmail.com>
2026-07-04 22:57:18 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,24 +1,16 @@
 password = input("Enter a password: ")
-
 score = 0
-
 if len(password) >= 8:
     score += 1
-
 if any(c.isupper() for c in password):
     score += 1
-
 if any(c.islower() for c in password):
     score += 1
-
 if any(c.isdigit() for c in password):
     score += 1
-
 special = "!@#$%^&*()-_=+[]{}|;:',.<>?/"
-
 if any(c in special for c in password):
     score += 1
-
 if score <= 2:
     print("Weak Password")
 elif score == 3 or score == 4:
